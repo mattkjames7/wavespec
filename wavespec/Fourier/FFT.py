895260b98aa776551f7ee3844ee1ca18981f6d72
mattkjames7 <mattkjames7@gmail.com>
2020-01-13 21:43:34 +0000

    updated FFT

--- wavespec/Fourier/FFT.py
+++ wavespec/Fourier/FFT.py
@@ -1,7 +1,27 @@
 import numpy as np
-from .WindowFunctions import 
+from .WindowFunctions import ApplyWindowFunction
 
 def FFT(t,x,WindowFunction=None,Params=None):
 
-
+	#find out the array length (l) and the interval between each value (i)
+	l = np.size(v)
+	i = t[1] - t[0]
 	
+	#firstly apply the window function to the time series
+	v = ApplyWindowFunction(x,WindowFunction,Params)
+	
+	#now to do the FFT
+	f = np.fft.fft(v)/l
+	fi = np.imag(f)
+	fr = np.real(f)
+
+	#calculate power
+	power = (fi**2 + fr**2)
+	
+	#frequency
+	freq = np.arange(l+1,dtype='float32')/(np.float32(l*i))
+	
+	#phase in radians
+	phase = np.arctan2(fi,fr)
+	
+	return (power,phase,freq,fr,fi)
