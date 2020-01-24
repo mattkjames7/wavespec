f484ed7e0707c8a10e0f03c103f3a82161555b42
mattkjames7 <mattkjames7@gmail.com>
2020-01-24 11:53:42 +0000

    updated version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wavespec",
-    version="0.0.1",
+    version="0.0.2",
     author="Matthew Knight James",
     author_email="mattkjames7@gmail.com",
     description="Some spectral analysis tools for analyzing waves in data.",
