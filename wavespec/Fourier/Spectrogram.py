8ba5df4a3b6b2e6ed06b037b800bba16d73c6ecf
mattkjames7 <mattkjames7@gmail.com>
2022-06-16 14:19:20 +0100

    started altering spectrogram code

--- wavespec/Fourier/Spectrogram.py
+++ wavespec/Fourier/Spectrogram.py
@@ -19,43 +19,44 @@ def Spectrogram(t,v,wind,slip,**kwargs):
 			next - when slip < wind, each window will have an overlap,
 			when slip > wind, there will be gaps where some data will be 
 			unused and when slip == wind, each window is adjacent in time.
-	 Freq : a list of frequencies (Hz) to solve for - only does anything
-			when using L-S
-	Method : Currently either 'FFT' or 'LS'
-	WindowFunction : Select a window function to apply to the data before 
-			the transform, the options are: 'none','cosine-bell','hamming',
-			'triangle','welch','blackman','nuttall','blackman-nuttall',
-			'flat-top','cosine','gaussian'			
-	Param : This parameter is used to alter some of the window functions
+	
+	Keyword Arguments
+	=================
+	WindowFunction : None|str
+			Select a window function to apply to the data before the 
+			transform, the options are: 
+			'none','cosine-bell','hamming','triangle','welch',
+			'blackman','nuttall','blackman-nuttall','flat-top','cosine',
+			'gaussian'			
+	Param : float
+			This parameter is used to alter some of the window functions
 			(see WindowFunctions.py).
-	Detrend : This will linearly detrend each time window before the 
-			transform.
-	FindGaps : This tells the routine to scan for data gaps, when set
-			to False - the data are assumed to be perfect.
-	GoodData : This can be set to a boolean array which tells the DetectGaps
-			function which data points are good (True) or bad (False),
-			if set to None, then any non-finite data is assumed to be bad.
-	Quiet : When set to True, the function produces no stdout output; when
-			False, stdout shows the progress.
-	LenW : This can be set to an integer value in order to force a specific
-			window length (the number of elements, as opposed to the length
-			in time defined using wind)
+	Detrend : bool|int
+			This will detrend using a polynomial (the degree of which is
+			given by the Detrend keyword, e.g. Detrend=2 for quadratic)
+			each time window before the transform.
+	GoodData : bool
+			This can be set to a boolean array which tells the function 
+			which data points are good (True) or bad (False), if set to 
+			None, then any non-finite data is assumed to be bad.
+	Quiet : bool
+			When set to True, the function produces no stdout output; 
+			when False, stdout shows the progress.
+#	LenW : 	#
+			#This can be set to an integer value in order to force a specific
+			#window length (the number of elements, as opposed to the length
+			#in time defined using wind)
 	Threshold:	If set to a value above 0, then all values which 
 			correspond to frequencies where the amplitude is less than
 			Threshold are set to 0, effectively removing noise from the
 			spectra.
-	Fudge:	(LS Only!)
-			This applies a fudge for when f == Nyquist frequency, because
-			small floating point numbers have relatively large errors.
-			This should only be needed if intending to reproduce a
-			two-sided FFT (also, if doing this then divide A by 2 and P 
-			by 4).
 	OneSided: (FFT Only!)
 			This should be set to remove the negative frequencies in
 			the second half of the spectra. In doing so, the amplitudes
 			are doubled and the powers are quadrupled.
-	Tax :	(LS only)
-			An array of times at the centre of each bin.
+	Tax :	float
+			An array of times at the centre of each bin. This will force
+			the program to use a specific set of windows.
 									
 	Returns
 	=======
